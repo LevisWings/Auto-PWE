9ccb35c688436b24b6f69cd77a87407d6495e4d0
LevisWings <AleWorkAndPersonalEmail@gmail.com>
2021-01-29 10:08:05 -0300

    Fixed redshift problem.

--- Auto-PWE.sh
+++ Auto-PWE.sh
@@ -116,6 +116,7 @@ function updates_directories_dependencies(){
 	sudo apt-get install libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev -y
 	sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
 	sudo apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
+    sudo apt purge redshift redshift-gtk -y > /dev/null 2>&1
 }
 
 function bspwm_and_sxhkd(){
